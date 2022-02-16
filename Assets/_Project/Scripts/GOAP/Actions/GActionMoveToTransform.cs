b6338b3944d841c7aa6867cc9eb7f83186c369cf
Stuart Millman <stu.millman15@gmail.com>
2022-02-16 20:17:12 +0000

    Added GActionMoveToVector + GActionMoveToTransform. Created ActionList + Action scriptableobjects.

---
+++ Assets/_Project/Scripts/GOAP/Actions/GActionMoveToTransform.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace stuartmillman.dissertation.goap.Actions
+{
+    [CreateAssetMenu(menuName = "GOAP/Actions/Move To (Transform)")]
+    public class GActionMoveToTransform : GAction
+    {
+        public override void Run(GAgent agent)
+        {
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/GOAP/Actions/GActionMoveToVector.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace stuartmillman.dissertation.goap.Actions
+{
+    [CreateAssetMenu(menuName = "GOAP/Actions/Move To (Vector)")]
+    public class GActionMoveToVector : GAction
+    {
+        public override void Run(GAgent agent)
+        {
+        }
+    }
+}
