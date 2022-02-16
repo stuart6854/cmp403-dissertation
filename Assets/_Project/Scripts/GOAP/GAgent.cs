6bf076eb39f2343f11f0c391858feff0208df182
Stuart Millman <stu.millman15@gmail.com>
2022-02-16 20:16:20 +0000

    Added GAgent.

---
+++ Assets/_Project/Scripts/GOAP/GAgent.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using UnityEngine;
+
+namespace stuartmillman.dissertation.goap
+{
+    public class GAgent : MonoBehaviour
+    {
+        [SerializeField] private GActionList actionList;
+
+        private readonly GState _initialState = new GState();
+        private readonly GState _goalState = new GState();
+
+        private GActionList _actionList;
+
+        private GAction[] _actionPlan;
+
+        private void Start()
+        {
+            _actionList = actionList.Clone();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void ClearInitialState()
+        {
+            _initialState.Clear();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stateName"></param>
+        /// <param name="value"></param>
+        public void AddInitialState(string stateName, object value)
+        {
+            _initialState.Set(stateName, value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void ClearGoals()
+        {
+            _goalState.Clear();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="stateName"></param>
+        /// <param name="value"></param>
+        public void AddGoal(string stateName, object value)
+        {
+            _goalState.Set(stateName, value);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Plan()
+        {
+            var newPlan = GPlanner.Plan(_actionList, _initialState, _goalState);
+            if (newPlan.Length == 0)
+            {
+                // Failed to create plane
+            }
+
+            _actionPlan = newPlan;
+        }
+    }
+}
