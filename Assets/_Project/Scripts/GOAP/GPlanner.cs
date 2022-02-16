01d760d3e20a6e4fd8454f82f7c8439705e0a5a5
Stuart Millman <stu.millman15@gmail.com>
2022-02-16 20:15:37 +0000

    Added GPlanner skeleton.

---
+++ Assets/_Project/Scripts/GOAP/GPlanner.cs
@@ -0,0 +1,20 @@
+﻿namespace stuartmillman.dissertation.goap
+{
+    /// <summary>
+    /// Brains of the GOAP system.
+    /// </summary>
+    public class GPlanner
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="actionList"></param>
+        /// <param name="initialState">The initial world/agent state</param>
+        /// <param name="goalState">The goal state</param>
+        /// <returns>Array of GAction for agent to carry out</returns>
+        public static GAction[] Plan(GActionList actionList, GState initialState, GState goalState)
+        {
+            return null;
+        }
+    }
+}
